ae5f3bb36b68a81ecd286d906853fdb1c9077d2b
彭双塔 <arthur@soundbus.cn>
2019-03-27 15:00:22 +0800

    1、上传PSTFramework库。

---
+++ Framework/PSTFramework.framework/Headers/HelloWorld.h
@@ -0,0 +1,17 @@
+//
+//  HelloWorld.h
+//  PSTFramework
+//
+//  Created by 彭双塔 on 2019/3/27.
+//  Copyright © 2019年 pst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HelloWorld : NSObject
++(NSString *)sayHellow;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Framework/PSTFramework.framework/Headers/PSTFramework.h
@@ -0,0 +1,19 @@
+//
+//  PSTFramework.h
+//  PSTFramework
+//
+//  Created by 彭双塔 on 2019/3/27.
+//  Copyright © 2019年 pst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PSTFramework.
+FOUNDATION_EXPORT double PSTFrameworkVersionNumber;
+
+//! Project version string for PSTFramework.
+FOUNDATION_EXPORT const unsigned char PSTFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSTFramework/PublicHeader.h>
+#import <PSTFramework/HelloWorld.h>
+
